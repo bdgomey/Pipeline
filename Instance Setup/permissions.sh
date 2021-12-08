49ce80a7e4c6cd834f338aaf4cca3ad7f7f319e4
Brian <brian.james.gomes@gmail.com>
2021-12-08 15:48:53 -0600

    fixed docker file

--- Instance Setup/permissions.sh
+++ Instance Setup/permissions.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo usermod -aG docker $USER
 sudo usermod -aG docker jenkins
+sudo chown jenkins:jenkins -R /var/lib/jenkins/workspace
 export PATH=/opt/maven/bin:$PATH
 sudo service jenkins restart
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
