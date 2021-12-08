4092b9901c87c4dff275c6d95ff83dbab0b2e7bd
Brian <brian.james.gomes@gmail.com>
2021-12-08 17:02:35 -0600

    added kube config file

--- Instance Setup/permissions.sh
+++ Instance Setup/permissions.sh
@@ -4,4 +4,6 @@ sudo usermod -aG docker jenkins
 sudo chown jenkins:jenkins -R /var/lib/jenkins/workspace
 export PATH=/opt/maven/bin:$PATH
 sudo service jenkins restart
+mkdir ~/.kube
+cp config.yaml ~/.kube
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
